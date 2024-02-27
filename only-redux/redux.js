2f0922ccaddf79520556aea67a6212c119c3c9c0
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-02-27 08:03:12 +0545

    setting up mupltiple actions with return

--- only-redux/redux.js
+++ only-redux/redux.js
@@ -8,6 +8,8 @@ const reducer = (store = INITIAL_VALUE, action) => {
   let newStore = store;
   if (action.type === "INCREMENT") {
     newStore = { counter: store.counter + 1 };
+  } else if (action.type === "DECREMENT") {
+    newStore = { counter: store.counter - 1 };
   }
   return newStore;
 };
@@ -22,6 +24,6 @@ const subscriber = () => {
 store.subscribe(subscriber);
 
 store.dispatch({ type: "INCREMENT" });
-store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
 store.dispatch({ type: "INCREMENT" });
 store.dispatch({ type: "INCREMENT" });
