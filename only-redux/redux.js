8dcecd954939f89c8c8fc8aa12bea65325ce90e3
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-02-27 08:10:40 +0545

    added ADDITION action

--- only-redux/redux.js
+++ only-redux/redux.js
@@ -10,6 +10,8 @@ const reducer = (store = INITIAL_VALUE, action) => {
     newStore = { counter: store.counter + 1 };
   } else if (action.type === "DECREMENT") {
     newStore = { counter: store.counter - 1 };
+  } else if (action.type === "ADDITION") {
+    newStore = { counter: store.counter + action.payload.number };
   }
   return newStore;
 };
@@ -26,4 +28,5 @@ store.subscribe(subscriber);
 store.dispatch({ type: "INCREMENT" });
 store.dispatch({ type: "DECREMENT" });
 store.dispatch({ type: "INCREMENT" });
-store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "ADDITION", payload: { number: 7 } });
+store.dispatch({ type: "DECREMENT" });
